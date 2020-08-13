e8ff3873aaf1a42e5acb9026928a5b6ab3669fc5
Viviana Damiano <62727615+vvndmn@users.noreply.github.com>
2020-08-12 17:08:09 -0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import * as serviceWorker from './serviceWorker';
+import App from './components/App/App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
